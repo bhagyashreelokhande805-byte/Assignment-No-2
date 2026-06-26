60899d3471d0ca9da4bd2dc45ef7d27e43683c93
bhagyashreelokhande805-byte <bhagyashreelokhande805@gmail.com>
2026-06-26 16:50:19 +0530

    Add files via upload

---
+++ ques no 2 assign no 9.py
@@ -0,0 +1,10 @@
+def chkGreater(a, b):
+    if a > b:
+        print("Greater number is", a)
+    else:
+        print("Greater number is", b)
+        
+        
+x = int(input("Enter first number: "))
+y = int(input("Enter second number: "))                           
+chkGreater(x, y)
---
+++ quesno 5 asign no 9.py
@@ -0,0 +1,8 @@
+num= int(input("Enter a number: "))
+
+if num %3==0:
+   num %5==0
+       
+   print("The number is divisible by 3 and 5")
+else:
+   print("the number is not divisible by 3 and 5")      
